fd61873841ba0d42ae311f743de4d2ae35a56cd5
DanielaFBL <48950358+DanielaFBL@users.noreply.github.com>
2019-05-23 11:42:24 -0500

    Create DanielaBarrios_Final.cpp

---
+++ DanielaBarrios_Final.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std; 
+
+int main(){
+    
+    float m = 7294.29;
+    float q = 2;
+    float omega=1.0;
+   
+    return 0; 
+}
+void leapfrog (float t0, float tf, float dt, float omega, string datos){
+
+  float t0=0.0;
+  float x=1.0;
+  float y=0.0;
+  float tf=10.0;
+  float dt=0.1;
+  float omega = 1.0;
+  float x0= 1.0;
+  float y0 = 0.0;
+    
+    
+  ofstream outfile;
+  outfile.open(datos.dat);
+    
+   
+      if(tf<3){
+          cout<<"E=0";
+      }
+      if (3>tf>7){
+          cout<<"E=(0,3)";
+      }
+  
+    
+  while(t0<tf){ 
+      
+    outfile << t0 << " " << x << " " << y << endl;
+    x = x + 0.5 * dt * y;
+    y  = y + dt * (-omega * omega * x);
+    t0 = t0 + dt;
+      
+     
+  outfile.close();
+}
